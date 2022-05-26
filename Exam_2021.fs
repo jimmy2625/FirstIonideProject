3c46f8c78b53dd5cf25e6ae052dab163bf36be9a
jimy <jimy@itu.dk>
2022-05-26 22:00:19 +0200

    med kommentarer på exam2021

--- Exam_2021.fs
+++ Exam_2021.fs
@@ -6,6 +6,8 @@ module Exam2021
 type direction = North | East | South | West
 type coord     = C of int * int
 
+//I match with the direction given and I write out the coordinate for readability
+
 let move (dist:int) (dir:direction) (C(x,y)) = 
     match dir with
     |North -> C(x,y-dist)
@@ -14,6 +16,8 @@ let move (dist:int) (dir:direction) (C(x,y)) =
     |West -> C(x-dist,y)
 
 move 10 North (C (0, 0))
+
+//I can now use the function keyword since i don't have to use a specific match with case
 
 let turnRight = function
 |North -> East
@@ -33,6 +37,11 @@ turnRight North;;
 type position = P of (coord * direction)
 type move     = TurnLeft | TurnRight | Forward of int
 
+(*
+    I match with the move case and employ my previously created turnLeft and turnRight functions, 
+    if the move is Forward, I will add a distance which I give as a parameter in move
+*)
+
 let step (P(coord,direction)) (m:move) = 
     match m with
     |TurnLeft -> (P(coord,turnLeft direction))
@@ -47,6 +56,11 @@ step (P (C (0, 0), North)) TurnLeft
 step (P (C (0, 0), North)) (Forward 10)
 
 //1.3
+(*
+    Instead of matching on the single move, I match on the move list
+    I make an end case which is the empty list which just returns the startposition
+    The list case runs recursively on each step in the list's tail (xs)
+*)
 let rec walk (P(coord,direction)) (ms : move list) = 
     match ms with 
     |[] -> (P(coord,direction))
@@ -56,6 +70,10 @@ walk (P (C (0, 0), North)) [TurnRight; Forward 10; TurnLeft]
 
 walk (P (C (0, 0), North)) []
 
+(*
+    I do the same as in walk but instead use the List.fold with the folder function "step" on the whole list ms
+*)
+
 let walk2 (P(coord,direction)) (ms:move list) =
     match ms with
     |[] -> (P(coord,direction))
@@ -64,6 +82,13 @@ let walk2 (P(coord,direction)) (ms:move list) =
 walk2 (P (C (0, 0), North)) [TurnRight; Forward 10; TurnLeft]
 
 //1.4
+(*
+    I match with the move list and create an end case which is the empty list that returns a list containing the start position
+    The next case in the pattern is for the list, where I do a nested match x with. Since TurnLeft and TurnRight both don't add any coordinates to the coord list,
+    i will just step towards the given direction.
+    If the match case hits the Forward case, the starting position (coord) will be the head of the coord list while the path runs recursively
+    with the move function and recursively adds coordinates to the tail of the list if the Forward case is reached.
+*)
 let rec path (P(coord,direction)) (ms : move list) : coord list =
         match ms with
         |[]     -> [coord] 
@@ -74,6 +99,21 @@ let rec path (P(coord,direction)) (ms : move list) : coord list =
 
 path (P(C(1,0), North)) []
 path (P (C (0, 0), North)) [TurnRight; Forward 10; TurnLeft]
+
+//1.5
+(*
+    To implement a tail-recursive function of path, I will first make the outer function non recursive but the inner recursive.
+    Afterwards, I will add an acc of type coord list to the pathInner and the end case and nested match cases are the same as in path
+    The match x case now uses the pathInner recursive function instead and adds an accumulator at the end for both TurnLeft, TurnRight and Forward
+*)
+
+//mit eksempel:
+let addOne (input : int list) : int list =
+    let rec addOneInner (input : int list) (acc : int list) : int list = 
+        match input with
+        | [] -> acc
+        | x :: xs -> addOneInner xs ((x + 1) :: acc)
+    addOneInner input []
 
 let path2 (P(coord,direction)) (ms : move list) : coord list =
     let rec pathInner (P(coord,direction)) (ms : move list) (acc : coord list) : coord list =
