4b9bfbf12aac35fb1c049c4bd87dbb4d0669e773
jimy <jimy@itu.dk>
2022-05-31 12:20:34 +0200

    med lidt 2021 re

--- Exam_2021Re.fs
+++ Exam_2021Re.fs
@@ -9,6 +9,26 @@ type binList<'a, 'b> =
 let rec length binlst = 
     match binlst with
     |Nil -> 0
-    |Cons1  -> length()
+    |Cons1 -> length(+1)
+    |Cons2 -> length(+1)
 
 length (Cons1 (3, Cons2 (true, Cons1 (4, Cons2 (false, Cons2(true, Nil))))))
+
+//2 Code Comprehension
+
+let rec foo xs ys =
+  match xs, ys with
+  | [], ys -> ys
+  | xs, [] -> xs
+  | x :: xs, y :: ys when x < y ->
+  	x :: (foo xs (y :: ys))
+  | x :: xs, y :: ys ->
+  	y :: (foo (x :: xs) ys)
+
+and bar =
+  function
+  | [] -> []
+  | [x] -> [x]
+  | xs ->
+    let (a, b) = List.splitAt (List.length xs / 2) xs
+    foo (bar a) (bar b)
